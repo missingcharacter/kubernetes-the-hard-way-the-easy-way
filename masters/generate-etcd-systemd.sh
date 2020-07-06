93b5b71e31813d5f9113c39bb0b3badf52da7b2b
Ricardo Rosales <missingcharacter@gmail.com>
2020-07-06 16:12:05 -0500

    generate-etcd-systemd script works now

--- masters/generate-etcd-systemd.sh
+++ masters/generate-etcd-systemd.sh
@@ -2,8 +2,31 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-INTERNAL_IP="$(hostname -I)"
+if [[ ! -x $(command -v etcd) || ! -x $(command -v etcdctl) ]]; then
+  wget -q --show-progress --https-only --timestamping \
+    "https://github.com/etcd-io/etcd/releases/download/v3.4.0/etcd-v3.4.0-linux-amd64.tar.gz"
+  tar -xvf etcd-v3.4.0-linux-amd64.tar.gz
+  sudo mv etcd-v3.4.0-linux-amd64/etcd* /usr/local/bin/
+  rm -rf etcd-v3.4.0-linux-amd64.tar.gz etcd-v3.4.0-linux-amd64/
+fi
+
+if [[ ! -f /etc/etcd/kubernetes.pem || ! -f /etc/etcd/kubernetes-key.pem ]]; then
+  sudo mkdir -p /etc/etcd /var/lib/etcd
+  sudo cp ca.pem kubernetes-key.pem kubernetes.pem /etc/etcd/
+fi
+
+INTERNAL_IPS=( $(hostname -I | tr '[:space:]' '\n') )
 ETCD_NAME="$(hostname -s)"
+VERSION_REGEX='([0-9]*)\.'
+
+declare -a COMPUTER_IPV4_ADDRESSES
+for ip in "${INTERNAL_IPS[@]}"; do
+  if grep -E "${VERSION_REGEX}" <<< "${ip}" > /dev/null; then
+    COMPUTER_IPV4_ADDRESSES+=("${ip}")
+  fi
+done
+
+echo 'Creating etcd systemd unit'
 
 cat <<EOF | sudo tee /etc/systemd/system/etcd.service
 [Unit]
@@ -22,12 +45,12 @@ ExecStart=/usr/local/bin/etcd \\
   --peer-trusted-ca-file=/etc/etcd/ca.pem \\
   --peer-client-cert-auth \\
   --client-cert-auth \\
-  --initial-advertise-peer-urls https://${INTERNAL_IP}:2380 \\
-  --listen-peer-urls https://${INTERNAL_IP}:2380 \\
-  --listen-client-urls https://${INTERNAL_IP}:2379,https://127.0.0.1:2379 \\
-  --advertise-client-urls https://${INTERNAL_IP}:2379 \\
+  --initial-advertise-peer-urls https://${COMPUTER_IPV4_ADDRESSES[0]}:2380 \\
+  --listen-peer-urls https://${COMPUTER_IPV4_ADDRESSES[0]}:2380 \\
+  --listen-client-urls https://${COMPUTER_IPV4_ADDRESSES[0]}:2379,https://127.0.0.1:2379 \\
+  --advertise-client-urls https://${COMPUTER_IPV4_ADDRESSES[0]}:2379 \\
   --initial-cluster-token etcd-cluster-0 \\
-  --initial-cluster controller-0=https://${INTERNAL_IP}:2380 \\
+  --initial-cluster ${ETCD_NAME}=https://${COMPUTER_IPV4_ADDRESSES[0]}:2380 \\
   --initial-cluster-state new \\
   --data-dir=/var/lib/etcd
 Restart=on-failure
@@ -36,3 +59,17 @@ RestartSec=5
 [Install]
 WantedBy=multi-user.target
 EOF
+
+echo 'Reloading systemd, enabling and starting etcd systemd service'
+
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd
+
+echo 'Listing etcd members'
+
+sudo ETCDCTL_API=3 etcdctl member list \
+  --endpoints=https://127.0.0.1:2379 \
+  --cacert=/etc/etcd/ca.pem \
+  --cert=/etc/etcd/kubernetes.pem \
+  --key=/etc/etcd/kubernetes-key.pem
